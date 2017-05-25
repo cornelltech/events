4262a54496d1229faf81e043798e5be5e2c74f43
auto push <rachel.sobel@cornell.edu>
2017-05-25 12:27:54 -0400

    changes to python to keep from pulling in bootstrap files

--- events_retriever.py
+++ events_retriever.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
 import contentful
+from flask import Flask, render_template, request
 import pdb
 
 SPACE_ID = 'g2o2lk221h21'
 ACCESS_TOKEN = 'd4d54c120ee06a69e46d8dc64e56e1703093d27524ae6006c8833bc10938ae0f'
 
 app = Flask(__name__)
-Bootstrap(app) # why isn't this code doing anything...
 
 @app.route('/')
 def index_page():
