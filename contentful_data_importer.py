6bf5b53d8cdd21b863ade29c3587c050211d2980
auto push <rachel.sobel@cornell.edu>
2017-05-30 18:08:23 -0400

    first pass on writing contentful data to files; does not currently write any data to files #woohoo

---
+++ contentful_data_importer.py
@@ -0,0 +1,41 @@
+from contentful import Client
+import datetime
+import json
+import jsonpickle
+import os
+import pdb
+import pytz
+import tzlocal
+
+SPACE_ID = 'g2o2lk221h21'
+ACCESS_TOKEN = 'd4d54c120ee06a69e46d8dc64e56e1703093d27524ae6006c8833bc10938ae0f'
+CONTENT_ID_EVENT = 'event'
+ORDER_RESULTS_BY = '-sys.updatedAt' # most recently updated first
+LIMIT = 3
+
+
+
+client = Client(SPACE_ID, ACCESS_TOKEN)
+
+entries = client.entries({'order': ORDER_RESULTS_BY, 'content_type': CONTENT_ID_EVENT})
+
+for entry in entries:
+    filename = 'json/' + entry.sys['id'] + '.json'
+    entry_modified = entry.sys['updated_at']
+    if (os.path.isfile(filename)):
+        file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filename), tzlocal.get_localzone())
+        if(int((entry_modified - file_modified).total_seconds()) == 0):
+            print 'actually you have read all this stuff already'
+            break
+
+    print entry.event_title
+    print entry.sys['id']
+
+    with open(filename, 'w') as outfile:
+        json.dumps(jsonpickle.encode(entry), outfile)
+        # this is really hacky #woohoo
+        contentful_tz = entry_modified.tzinfo
+        era_zero = datetime.datetime(1970,1,1).replace(tzinfo=contentful_tz)
+        in_seconds = (entry_modified - era_zero).total_seconds()
+        os.utime(filename, (in_seconds, in_seconds))
+        print os.path.getmtime(filename)
