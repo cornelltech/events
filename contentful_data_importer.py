38b07c3aaf4c00bf415b60aa10b9d8085e658038
auto push <rachel.sobel@cornell.edu>
2017-06-06 11:27:02 -0400

    comments and code tidiness

--- contentful_data_importer.py
+++ contentful_data_importer.py
@@ -31,33 +31,40 @@ def process_entries(client):
             'content_type': CONTENT_ID_EVENT,
             'limit': LIMIT,
             'skip': skip})
-        print 'skip= ', skip, ' limit=', LIMIT
+
         for entry in entries:
             filename = 'json/' + entry.sys['id'] + '.json'
             entry_modified = entry.sys['updated_at']
             if (os.path.isfile(filename)):
-                file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(filename), tzlocal.get_localzone())
+                file_modified = \
+                    datetime.datetime.fromtimestamp(os.path.getmtime(filename),
+                                                    tzlocal.get_localzone())
+
+                # if we reach an entry we've already processed, the remaining
+                # data on the server is older than our stored data
                 if(int((entry_modified - file_modified).total_seconds()) == 0):
                     print 'stopping at: ', entry.event_title, '(', filename, ')'
-                    print 'the remaining content was processed on an earlier run'
+                    print 'remaining content was processed on an earlier run'
                     return
 
             with open(filename, 'w') as outfile:
                 json.dump(jsonpickle.encode(entry), outfile)
-                # this is really hacky #woohoo
+                # TODO: py3 might make these calculations nicer
                 contentful_tz = entry_modified.tzinfo
-                era_zero = datetime.datetime(1970,1,1).replace(tzinfo=contentful_tz)
+                era_zero = datetime.datetime(1970,1,1).replace(
+                                                        tzinfo=contentful_tz)
                 in_seconds = (entry_modified - era_zero).total_seconds()
                 outfile.close()
                 os.utime(filename, (in_seconds, in_seconds))
 
-                print 'saving file: ', entry.event_title, ' id=', entry.sys['id']
+                print 'saving file: ', entry.event_title, 'id=', entry.sys['id']
 
-        if (len(entries) < LIMIT):
+        if (len(entries) < LIMIT): # when you reach the final page
             print 'you have processed all the data'
             return
         else:
             skip += LIMIT
 
-client = Client(SPACE_ID, ACCESS_TOKEN)
-process_entries(client)
+if __name__ == '__main__':
+    client = Client(SPACE_ID, ACCESS_TOKEN)
+    process_entries(client)
