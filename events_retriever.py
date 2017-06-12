6f6be176dc03f7001d5f0a5f81ed9adbb9d8a98c
auto push <rachel.sobel@cornell.edu>
2017-06-12 17:43:13 -0400

    update events_retriever to read from files instead of from contentful server

--- events_retriever.py
+++ events_retriever.py
@@ -1,34 +1,17 @@
 import contentful
+import contentful_datareader
 from flask import Flask, render_template, request
-import pdb
 import os
+import pdb
 
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-try:
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-except Exception as e:
-    print "\nMissing .env file\n"
-
-SPACE_ID = os.environ.get('SPACE_ID', None)
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', None)
+CONTENT_ID_EVENT = 'event'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index_page():
-    return render_template('events_tiles.html', events=get_events())
-
-def get_events():
-    client = contentful.Client(SPACE_ID, ACCESS_TOKEN)
-    entries = client.entries()
-    events = []
-    for entry in entries:
-        if(entry.content_type.id == 'event'):
-            events.append(entry)
-    return events
+    return render_template('events_tiles.html',
+                    events=contentful_datareader.load_entries(CONTENT_ID_EVENT))
 
 if __name__ == '__main__':
     app.run()
