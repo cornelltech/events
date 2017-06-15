c5f4bc45507b35d62f84b60e17343026fcec68bc
Adrian Vatchinsky <avatchinsky@outlook.com>
2017-06-15 13:15:59 -0400

    filtering on categories + tags

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,83 @@
 ;( function( $, window, document, undefined ) {
     "use strict";
 
-    function filterOn(val) {
-        if( val === '*' ) {
-            $('.grid-item').show();
-        }else {
-            $('.grid-item').show();
-            $('.grid-item:not(' + val + ')').hide();
-        }
+    var filters = {
+        categories: '*',
+        tags: [ ]
+    };
 
+
+    /** Helper functions */
+
+    /**
+     * function to normalize text
+     * @param {string} text
+     */
+    function normalizeText(text) {
+        return text.toLocaleLowerCase().replace(' ', '_');
+    }
+
+    /**
+     * Show all the grid item tiles
+     */
+    function showAll() {
+        $('.grid-item').show();
+    }
+
+    /**
+     * Hide all teh grid item tiles
+     */
+    function hideAll() {
+        $('.grid-item').hide();
+    }
+
+    /**
+     * Sets the category value to the filters
+     * @param {string} category
+     */
+    function setFilterCategory(category) {
+        filters.categories = normalizeText(category);
+    }
+
+    /**
+     * Sets the tag value to the filters, dedupes values as well
+     * @param {string[]} tag 
+     */
+    function setFilterTags(tags) {
+        filters.tags = $.map( tags, function( elem ) {
+            return '.tag-' + normalizeText(elem);
+        });
+    }
+
+    function filterItems() {
+        hideAll(); // reset all objects
+
+        // hide everything that matches the category filter
+        var focusedItems;
+
+        if( filters.categories !== '*' ) {
+            focusedItems = $('.grid-item' + filters.categories);
+        } else {
+            focusedItems = $('.grid-item');
+        };
+        
+        // show only the tags which we are filtering on
+        $.each(filters.tags, function( index, value ) {
+            focusedItems = focusedItems.filter(value);
+        });
+
+        focusedItems.show();
+        
+    }
+
+    /** Initializer functions */
+
+    function initCategories() {
+        $('.filter-btn').click(function(evt) {
+            var filterValue = evt.target.getAttribute('data-filter-value');
+            setFilterCategory(filterValue);
+            filterItems();
+        });
     }
 
     function initSelect() {
@@ -16,12 +85,14 @@
             placeholder: 'Filter by tag',
         });
         $('select').on("select2:select", function (e) { 
-            var tag = e.target.value;
-            filterOn('.' + tag);
+            var tags = $('select').val();
+            setFilterTags(tags);
+            filterItems();
         });
         $('select').on("select2:unselect", function (e) { 
-            var tag = e.target.value;
-
+            var tags = $('select').val();
+            setFilterTags(tags);
+            filterItems();
         });
 
     }
@@ -34,16 +105,11 @@
         });
     }
 
-    /** Attach event listeners */
-
-    $('.filter-btn').click(function(evt) {
-        var filterValue = evt.target.getAttribute('data-filter-value');
-        filterOn(filterValue);
-    });
-
 
     /** Main */
+
     function main(){
+        initCategories();
         initSelect();
     } 
     main();
