8c9df5cb08d6021f22cc33bb42feaafa8ccec3c0
Adrian Vatchinsky <avatchinsky@outlook.com>
2017-06-16 16:19:05 -0400

    freezer config

--- freezer.py
+++ freezer.py
@@ -2,6 +2,7 @@ from flask_frozen import Freezer
 from events_retriever import app
 
 app.config['FREEZER_DESTINATION'] = 'docs'
+app.config['FREEZER_RELATIVE_URLS'] = True
 
 freezer = Freezer(app)
 
