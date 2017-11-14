e8220dbe04d3aa7b6eb00955559b7f577e390ab1
Rachel Sobel <rachel.sobel@cornell.edu>
2017-11-14 16:32:53 -0500

    excruciating timezone math

--- events_retriever.py
+++ events_retriever.py
@@ -1,8 +1,11 @@
 import contentful
 import contentful_datareader
+import datetime
+import dateutil
 from flask import Flask, render_template, request
 import os
 import pdb
+import pytz
 
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -31,9 +34,37 @@ def index_page():
 
 @app.route('/next2weeks.html')
 def next_up():
+
+    def naive(d):
+        return d.tzinfo is None or d.tzinfo.utcoffset(d) is None
+
     events, tags = get_events()
-    return render_template('next2weeks.html', events=events, tags=tags)
-    
+
+    now = datetime.datetime.now(pytz.utc)
+    future_cutoff = now + datetime.timedelta(days=14)
+
+    # print 'now', naive(now), 'future cutoff', naive(future_cutoff)
+    # # pdb.set_trace()
+    #
+    # soon_events = []
+    # for event in events:
+    #     print 'naive? ', naive(event.start_time)
+    #     # pdb.set_trace()
+    #     print event.start_time
+    #     print event.start_time.tzinfo
+    #     if event.start_time >= now:
+    #         if event.start_time <= future_cutoff:
+    #             soon_events.append(event)
+    soon_events = filter(lambda x: x.start_time >= now and x.start_time <= future_cutoff, events)
+
+    for event in soon_events:
+        print event.event_title
+        print event.start_time
+
+    # soon_events = filter(lambda)
+
+    return render_template('next2weeks.html', events=soon_events, tags=tags)
+
 def get_events():
     client = contentful.Client(SPACE_ID, ACCESS_TOKEN)
     entries = client.entries()
@@ -42,6 +73,8 @@ def get_events():
     for entry in entries:
 
         if(entry.content_type.id == 'event'):
+            # standardize contentful's bad timezone handling
+            entry.start_time = entry.start_time.replace(tzinfo=pytz.utc)
             events.append(entry)
 
         if(entry.content_type.id == 'tag'):
